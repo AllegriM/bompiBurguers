259e2230610e5f1b0981d7a518fd36d6beeeed08
Marco Allegri <elmonky99@gmail.com>
2022-04-07 19:01:27 -0300

    ordering cart in delivery index added & added ordering screen to implement DOM in the future!!

--- js/main.js
+++ js/main.js
@@ -6,78 +6,58 @@ const precioPapas = 500;
 const combo = 1200;
 // Mientras el valor sea null/Not a Number/ mayor a 4 y menor o igual a 0 volver a preguntar
 
-const reorder = () =>{
-    let reorder = parseInt(prompt("Le gustaria pedir algo mas? \n1:Si \n2:No"));
-    while (reorder === null || reorder === NaN || reorder >= 3 || reorder <= 0) {
-        alert("Esa opcion no es valida!");
-        reorder = parseInt(prompt("Le gustaria volver a pedir? \n1:Si \n2:No"));
-    }
-    if(reorder == 1){
-        pedidoMenu();
-    }else if (reorder == 2) {
-        return alert("No hay problema!! Vuelva pronto!!! \n\n Esta es su factura:");
-    }else{
-        alert("Opcion no valida! Las unicas respuestas validas son numericas: \n1:Si \n2:No ");
-    }
-}
+// const reorder = () =>{
+//     let reorder = parseInt(prompt("Le gustaria pedir algo mas? \n1:Si \n2:No"));
+//     while (reorder === null || reorder === NaN || reorder >= 3 || reorder <= 0) {
+//         alert("Esa opcion no es valida!");
+//         reorder = parseInt(prompt("Le gustaria volver a pedir? \n1:Si \n2:No"));
+//     }
+//     if(reorder == 1){
+//         pedidoMenu();
+//     }else if (reorder == 2) {
+//         return alert("No hay problema!! Vuelva pronto!!! \n\n Esta es su factura:");
+//     }else{
+//         alert("Opcion no valida! Las unicas respuestas validas son numericas: \n1:Si \n2:No ");
+//     }
+// }
 
-const pedidoMenu = () => {
-    let order = parseInt(prompt("Que te gustaria pedir? \n1:Hamburguesa \n2:Papas \n3:Ambos \n4:Salir"));
-    console.log(order);
-    let quantity;
-    let subtotal0 = 0;
-    let subtotal1 = 0;
-    let subtotal2 = 0;
-    while (order === null || order === NaN || order >= 5 || order <= 0) {
-        alert("Esa opcion no es valida!")
-        order = parseInt(prompt("Que te gustaria pedir? \n1:Hamburguesa \n2:Papas \n3:Ambos \n4:Salir"));
-    }
-    if (order == 1) {
-        quantity = parseInt(prompt("Cuantas hamburguesas le gustaria pedir?"));
-        alert(`Pidio ${quantity} hamburguesa/s, su costo es de $${quantity * precioHamburguesa}`);
-        subtotal0 += (quantity * precioHamburguesa);
-    } else if (order == 2) {
-        quantity = parseInt(prompt("Cuantas papas le gustaria pedir?"));
-        alert(`Pidio ${quantity} papa/s, su costo es de $${quantity * precioPapas}`);
-        subtotal1 += (quantity * precioPapas);
-    } else if (order == 3) {
-        quantity = parseInt(prompt("Cuantos combos le gustaria pedir?"));
-        alert(`Eligio ${quantity} combo/s, su costo es de $${quantity * (precioHamburguesa + precioPapas)}`);
-        subtotal2 += (quantity * combo);
-        // order = (prompt("Que mas le gustaria pedir? \n1:Hamburguesa \n2:Papas \n3:Ambos \n4:Salir"));
-    } else if (order == 4){
-        return alert("Gracias!! Vuelva pronto!!!");;
-    } else {
-        alert ("");
-    } 
-    reorder();
-    let total = subtotal0 + subtotal1 + subtotal2;
-    alert(`Total: \nHamburguesas: $${subtotal0}. \nPapas: $${subtotal1}.  \nCombos: $${subtotal2}. \n\nTotal: $${total}.`);
-}
+// const pedidoMenu = () => {
+//     let order = parseInt(prompt("Que te gustaria pedir? \n1:Hamburguesa \n2:Papas \n3:Ambos \n4:Salir"));
+//     console.log(order);
+//     let quantity;
+//     let subtotal0 = 0;
+//     let subtotal1 = 0;
+//     let subtotal2 = 0;
+//     while (order === null || order === NaN || order >= 5 || order <= 0) {
+//         alert("Esa opcion no es valida!")
+//         order = parseInt(prompt("Que te gustaria pedir? \n1:Hamburguesa \n2:Papas \n3:Ambos \n4:Salir"));
+//     }
+//     if (order == 1) {
+//         quantity = parseInt(prompt("Cuantas hamburguesas le gustaria pedir?"));
+//         alert(`Pidio ${quantity} hamburguesa/s, su costo es de $${quantity * precioHamburguesa}`);
+//         subtotal0 += (quantity * precioHamburguesa);
+//     } else if (order == 2) {
+//         quantity = parseInt(prompt("Cuantas papas le gustaria pedir?"));
+//         alert(`Pidio ${quantity} papa/s, su costo es de $${quantity * precioPapas}`);
+//         subtotal1 += (quantity * precioPapas);
+//     } else if (order == 3) {
+//         quantity = parseInt(prompt("Cuantos combos le gustaria pedir?"));
+//         alert(`Eligio ${quantity} combo/s, su costo es de $${quantity * (precioHamburguesa + precioPapas)}`);
+//         subtotal2 += (quantity * combo);
+//         // order = (prompt("Que mas le gustaria pedir? \n1:Hamburguesa \n2:Papas \n3:Ambos \n4:Salir"));
+//     } else if (order == 4){
+//         return alert("Gracias!! Vuelva pronto!!!");;
+//     } else {
+//         alert ("");
+//     } 
+//     reorder();
+//     let total = subtotal0 + subtotal1 + subtotal2;
+//     alert(`Total: \nHamburguesas: $${subtotal0}. \nPapas: $${subtotal1}.  \nCombos: $${subtotal2}. \n\nTotal: $${total}.`);
+// }
 
 
-console.log(pedidoMenu());
+// console.log(pedidoMenu());
 // const buyBurguer = () =>{
 
 // }
 
-// Agregar productos al Carrito.
-
-
-class Producto {
-    constructor (nombre, tipo, precio){
-        this.nombre = nombre;
-        this.tipo = tipo; 
-        this.precio = precio;
-    }
-}
-
-let carritoOrdenes = [];
-
-const addProduct = () =>{
-    let nombre = prompt("Cual es el nombre de su producto?");
-    let tipo = prompt("Que tipo de producto es?");
-    let precio = prompt("Que precio tiene su producto?");
-    const nuevoProducto = new Producto(nombre, tipo, precio);
-    carritoOrdenes.push(nuevoProducto);
-}
---
+++ js/orderCart.js
@@ -0,0 +1,105 @@
+// Agregar productos al Carrito.
+
+//*********  Clases  *********//
+
+class Producto {
+    constructor(id, name, type, price) {
+        this.id = parseInt(id);
+        this.name = name.toUpperCase();
+        this.type = type.toUpperCase();
+        this.price = parseInt(price);
+    }
+    cuponDescuento() {
+        if (this.price > 3000) {
+            let valorFinal = this.price - (this.price * 0.15);
+            return valorFinal;
+        } else {
+            return alert("No superaste los $3000. No hay descuento esta vez")
+        }
+    }
+}
+
+
+//********* Productos *********//
+
+// Hamburguesas
+const neuquen = new Producto(1, "NEUQUEN", "HAMBURGUESA", 780);
+const cordoba = new Producto(2, "CORDOBA", "HAMBURGUESA", 750);
+const mendoza = new Producto(3, "MENDOZA", "HAMBURGUESA", 800);
+const santaFe = new Producto(4, "SANTA FE", "HAMBURGUESA", 780);
+const laRioja = new Producto(5, "LA RIOJA", "HAMBURGUESA", 770);
+const santaCruz = new Producto(6, "SANTA CRUZ", "HAMBURGUESA", 800);
+const jujuy = new Producto(7, "JUJY", "HAMBURGUESA", 750, 7);
+const sanJuan = new Producto(8, "SAN JUAN", "HAMBURGUESA", 770);
+const buenosAires = new Producto(9, "BUENOS AIRES", "HAMBURGUESA", 850);
+const corrientes = new Producto(10, "CORRIENTES", "HAMBURGUESA", 830);
+// Extras
+const papasComunes = new Producto(11, "PAPAS COMUNES", "EXTRAS", 600);
+const papasCheddar = new Producto(12, "PAPAS CON CHEDDAR", "EXTRAS", 680);
+const chickenDice = new Producto(13, "CHICKEN DICES", "EXTRAS", 700);
+const OnionRing = new Producto(14, "ONION RINGS", "EXTRAS", 650);
+// Ensaladas
+const caesar = new Producto(15, "CAESAR", "ENSALADAS", 700);
+const reySol = new Producto(16, "REY SOL", "ENSALADAS", 650);
+// bebidas sin alcohol
+const coca = new Producto(17, "COCA", "BEBIDAS S/ALC", 200);
+const cocaZero = new Producto(18, "COCA ZERO", "BEBIDAS S/ALC", 200);
+const agua = new Producto(19, "AGUA", "BEBIDAS S/ALC", 150);
+const pasoToros = new Producto(20, "TOROS POMELO", "BEBIDAS S/ALC", 200);
+// bebidas con alcohol
+const budweis = new Producto(21, "BUDWEISER", "BEBIDAS C/ALC", 260);
+const heineken = new Producto(22, "HEINEKEN", "BEBIDAS C/ALC", 350);
+const corona = new Producto(23, "CORONA", "BEBIDAS C/ALC", 260);
+const brahma = new Producto(24, "BRAHMA", "BEBIDAS C/ALC", 250);
+
+
+
+//********* Variables *********//
+
+let orderCart = [];
+//============================== 0 ===============================================  1  ============================== 2 =========================  3  =============================  4  ================== // 
+let orderStock = [neuquen, cordoba, mendoza, santaFe];
+
+let customerOrder;
+let quantity;
+let product;
+
+//********* Funciones *********//
+
+const addHamburguer = () => {
+    customerOrder = parseInt(prompt(`Bienvenido a Bompi Burger! Seleccione la hamburguesa que le gustaria comprar: \n\n1: ${orderStock[0].name} \n2: ${orderStock[1].name} \n3: ${orderStock[2].name} \n4: ${orderStock[3].name}`));
+    while (customerOrder >= 5 || customerOrder <= 0 || isNaN(customerOrder)) {
+        alert("Escoja un valor correcto de la lista anterior")
+        addHamburguer();
+    }
+    nombreProducto = orderStock[customerOrder - 1].name;
+    return nombreProducto;
+    // orderCart.push(nuevoProducto);
+    // return nuevoProducto;
+}
+
+const addQuantity = () => {
+    product = addHamburguer();
+    quantity = parseInt(prompt(`Cuantas unidades le gustaria comprar?`));
+    while (quantity < 0 || isNaN(quantity)) {
+        alert("Cantidad invalidad, seleccione una cantidad valida!");
+        quantity = parseInt(prompt(`Cuantas unidades le gustaria comprar?`));
+    }
+    let subTotalProducto = orderStock[customerOrder - 1].price * quantity;
+    orderCart.push({ product, quantity, subTotalProducto });
+    if (confirm`!Añadido correctamente! Queres agregar otro producto?\n\n En caso que quiera terminar de comprar y ver su factura aprete 'Cancel'` === true) {
+        addQuantity();
+    } else {
+        alert("Ahora podras ver tus productos añadidos al carrito en consola");
+    }
+}
+
+addQuantity();
+
+// const addExtra = () => {
+//     customerOrder = parseInt(prompt(`Bienvenido a Bompi Burger! Seleccione la hamburguesa que le gustaria consumir: \n\n\n1: ${orderStock[0].name} \n2: ${orderStock[1].name} \n3: ${orderStock[2].name} \n4: ${orderStock[3].name}`));
+//     if (customerOrder >= 5 || customerOrder <= 0) {
+//         alert("Escoja un valor correcto de la lista anterior")
+//         addHamburguer();
+//     }
+// }
